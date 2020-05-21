6356ab92cddf20d4b05dded9bf4ce756c7b4aa82
Robel Geda <rgeda@stsci.edu>
2020-05-21 14:34:33 -0400

    fix version control

--- stips/__init__.py
+++ stips/__init__.py
@@ -1,15 +1,21 @@
 from __future__ import absolute_import
 
+import os
+
 # make use of astropy affiliate framework to set __version__, __githash__, and
 # add the test() helper function
 
 from ._astropy_init import *
 
-__all__ = ['astro_image', 'errors', 'galaxy_module', 'instruments', 'observation_module', 'scene_module', 'stellar_module', 'utilities', 'version', 'test']
-# Don't modify the line above, or this line!
-import automodinit, os
-automodinit.automodinit(__name__, __file__, globals())
-del automodinit
+__all__ = ['astro_image', 'errors', 'galaxy_module', 'instruments',
+           'observation_module', 'scene_module', 'stellar_module',
+           'utilities', 'version', '__version__', 'test']
+
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = 'dev'
+version = __version__
 
 # Local Definitions
 
@@ -17,7 +23,7 @@ from .astro_image import AstroImage
 from .instruments import Instrument
 from .observation_module import ObservationModule
 from .scene_module import SceneModule
-from .version import __version__
+
 from .utilities import GetStipsData, internet, CachedJbtBackground, __grid__
 
 __grid__pandeia__version__ = __grid__.__pandeia__version__
