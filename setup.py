3b3823b1892d40922d7f929d05c91c14c83230b3
Sebastian <gmz.sebastian@gmail.com>
2022-01-25 12:34:39 -0500

    Fixed installation bug +stips

--- setup.py
+++ setup.py
@@ -8,16 +8,24 @@ import glob
 from setuptools import setup
 from setuptools.config import read_configuration
 
+from astropy_helpers.setup_helpers import register_commands, get_package_info
+from astropy_helpers.version_helpers import generate_version_py
+
 # Store the package name in a built-in variable so it's easy
 # to get from other parts of the setup infrastructure
 PACKAGENAME = read_configuration('setup.cfg')['metadata']['name']
+
+# Create a dictionary with setup command overrides. Note that this gets
+# information about the package (name and version) from the setup.cfg file.
+cmdclass = register_commands()
 
 # Freeze build information in version.py. Note that this gets information
 # about the package (name and version) from the setup.cfg file.
 version = read_configuration('setup.cfg')['metadata']['version']
 
-setup(
-        version=version, 
-        package_data={"stips": ['data/*']}#,
-#        scripts=glob.glob("stips/commands/*")
-)
+# Get configuration information from all of the various subpackages.
+# See the docstring for setup_helpers.update_package_files for more
+# details.
+package_info = get_package_info()
+
+setup(version=version, **package_info)
