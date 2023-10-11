1029b9763a27791fec28d4cc5fa98f1ea84b50d3
Sebastian <gmz.sebastian@gmail.com>
2023-10-11 17:38:42 -0400

    fixed import

--- stips/utilities/__init__.py
+++ stips/utilities/__init__.py
@@ -1,17 +1,17 @@
 __all__ = ['utilities']
 
 # Local Definitions
-from .utilities import (StipsEnvironment,
-						SetupDataPaths,
-						DownloadReferenceData,
-						GetStipsDataDir,
-						GetStipsData,
-						SelectParameter,
-						OffsetPosition,
-						InstrumentList,
-						read_metadata,
-						read_table,
-						rind,
-						sersic_lum,
-						get_pandeia_background,
-						StipsDataTable)
+from .utilities import StipsEnvironment, \
+						SetupDataPaths, \
+						DownloadReferenceData, \
+						GetStipsDataDir, \
+						GetStipsData, \
+						SelectParameter, \
+						OffsetPosition, \
+						InstrumentList, \
+						read_metadata, \
+						read_table, \
+						rind, \
+						sersic_lum, \
+						get_pandeia_background, \
+						StipsDataTable
